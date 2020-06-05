99965c92dd6927ed354c867e1fdfc892722e0fa4
Guigalaverna <guigalaverna@gmail.com>
2020-06-05 20:55:42 -0300

    Adicionado a pasta public

---
+++ public/scripts/upload-image.js
@@ -0,0 +1,7 @@
+const uploadButton = document.querySelector('button[name=image]')
+const uploadLabel = document.querySelector('label#upload-label')
+const inputFile = document.querySelector('input#upload-image')
+
+uploadLabel.addEventListener('click', () => {
+    
+})
